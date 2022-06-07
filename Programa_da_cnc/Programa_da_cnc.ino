d46adc9117ada36b99a049ad84a0122377318d3f
Gabriel <Gabriel.bolzani@hotmail.com>
2022-06-06 21:44:55 -0300

    Implementação serial e outros

--- Programa_da_cnc/Programa_da_cnc.ino
+++ Programa_da_cnc/Programa_da_cnc.ino
@@ -40,11 +40,15 @@ int y_feedRate = 100;
 int z_feedRate = 100;
 String CMD = "";
 double posicaoAtual[3];
-posicaoAtual[0]=0;
-posicaoAtual[1]=0;
-posicaoAtual[2]=0;
+posicaoAtual[0] = 0;
+posicaoAtual[1] = 0;
+posicaoAtual[2] = 0;
+
+String inputString = "";
+bool stringComplete = false;
 
 void setup() {
+  inputString.reserve(200);
   Serial.begin(115200);
   pinMode(LimitXAxes; INPUT_PULLUP);
   pinMode(LimitYAxes; INPUT_PULLUP);
@@ -59,62 +63,106 @@ void setup() {
   pinMode(Spindle; OUTPUT);
 }
 
-leitura_serial() {
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+  if (stringComplete) {
+    //Serial.println(inputString);
+    while (inputString[i] != ' ') {
+      cmd += inputString[i];
+      i++;
+    }
+    switch (inputString[0]) {
+      case 'G':
+        //Verificar se é g0, g1 ou g28
+        byte i = 0;
+
+        if (cmd == "G0") {
+
+        } else if (cmd == "G1") {
+
+        } else if (cmd == "G28") {
+          func_G28();
+        } else {
+          Serial.print("O comando ");
+          Serial.print(cmd);
+          Serial.println(" não é suportado! ");
+        }
+        break;
+      case '$':
+        if (cmd == "$help") {
+          //Chamar função de ajuda
+        } else if (cmd == "$$") {
+          //Chamar função Que moostra congiguração
+        } else if (cmd == "G28") {
+          //Chamar função que recebe configuração
+        } else {
+          Serial.print("O comando ");
+          Serial.print(cmd);
+          Serial.println(" não é suportado! ");
+        }
+        break;
+      default:
+        Serial.println("Sentença fora de padrão");
+        Serial.println("Digite $help para ajuda");
+        break;
+    }
+    inputString = "";
+  }
+  cmd = "";
+  stringComplete = false;
+}
+
+void func_G28 () {
+  digitalWrite(StepperEn, HIGH);
+  if (directionZ) {
+    digitalWrite(StepDir_Z, HIGH);
+  } else {
+    digitalWrite(StepDir_Z, LOW);
+  }
+  if (directionX) {
+    digitalWrite(StepDir_X, HIGH);
+  } else {
+    digitalWrite(StepDir_X, LOW);
+  }
+  if (directionY) {
+    digitalWrite(StepDir_Y, HIGH);
+  } else {
+    digitalWrite(StepDir_Y, LOW);
+  }
+  while (digitalRead(LimitZAxes) != LOW) {
+    digitalWrite(StepPusle_Z, HIGH);
+    delay(5);
+    digitalWrite(StepPusle_Z, LOW);
+    delay(5);
+  }
+  while ((digitalRead(LimitXAxes) != LOW) || (digitalRead(LimitYAxes) != LOW)) {
+    if (digitalRead(LimitXAxes) != LOW) {
+      digitalWrite(StepPusle_X, HIGH);
+      delay(5);
+      digitalWrite(StepPusle_X, LOW);
+      delay(5);
+    }
+    if (digitalRead(LimitYAxes) != LOW) {
+      digitalWrite(StepPusle_Y, HIGH);
+      delay(5);
+      digitalWrite(StepPusle_Y, LOW);
+      delay(5);
+    }
+  }
+  Serial.print("Movimento concluido\n");
+  posicaoAtual[0] = 0;
+  posicaoAtual[1] = 0;
+  posicaoAtual[2] = 0;
 }
 
 void loop() {
-  leitura_serial();
-  switch (cmd) {
-    case "G28"://Comando de Homming
-      digitalWrite(StepperEn, HIGH);
-      if (directionZ) {
-        digitalWrite(StepDir_Z, HIGH);
-      } else {
-        digitalWrite(StepDir_Z, LOW);
-      }
-      if (directionX) {
-        digitalWrite(StepDir_X, HIGH);
-      } else {
-        digitalWrite(StepDir_X, LOW);
-      }
-      if (directionY) {
-        digitalWrite(StepDir_Y, HIGH);
-      } else {
-        digitalWrite(StepDir_Y, LOW);
-      }
-      while (digitalRead(LimitZAxes) != LOW) {
-        digitalWrite(StepPusle_Z, HIGH);
-        delay(5);
-        digitalWrite(StepPusle_Z, LOW);
-        delay(5);
-      }
-      while ((digitalRead(LimitXAxes) != LOW) || (digitalRead(LimitYAxes) != LOW)) {
-        if (digitalRead(LimitXAxes) != LOW) {
-          digitalWrite(StepPusle_X, HIGH);
-          delay(5);
-          digitalWrite(StepPusle_X, LOW);
-          delay(5);
-        }
-        if (digitalRead(LimitYAxes) != LOW) {
-          digitalWrite(StepPusle_Y, HIGH);
-          delay(5);
-          digitalWrite(StepPusle_Y, LOW);
-          delay(5);
-        }
-      }
-      Serial.print("Movimento concluido\n");
-      posicaoAtual[0]=0;
-      posicaoAtual[1]=0;
-      posicaoAtual[2]=0;
-      break;
-    case "G0":
 
-      break;
-    case "G1":
-
-      break;
-    default:
-      Serial.print("Comando não reconhecido\n");
-      break;
-  }
 }
